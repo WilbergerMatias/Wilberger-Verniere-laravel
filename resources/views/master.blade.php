e67ea6d8b11ae365c5c146632549c3db8dc779ea
MartinVerniere <martincho7verniere@yahoo.com>
2024-07-26 14:58:27 -0300

    Correccion blades

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -7,7 +7,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         @vite(['resources/css/master.css'])
 
-
+        @yield('css')
         <title>WebCines</title>
     </head>
 
